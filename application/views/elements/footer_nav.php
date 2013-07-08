d652b385d9b564829729dbafedf99082f8065955
Juan Pablo [Lipibook] <m.juanpablob@gmail.com>
2013-07-07 20:23:45 -0400

    open pdf in a new window

--- application/views/elements/footer_nav.php
+++ application/views/elements/footer_nav.php
@@ -1,7 +1,7 @@
 <footer>
     <ul class="clearfix">
         <li><a href="<?php echo site_url(array('app', 'ranking')); ?>">Ranking</a></li>
-        <li><a href="<?php echo site_url('/media/privacidad.pdf'); ?>">Política de Privacidad</a></li>
+        <li><a href="<?php echo site_url('/media/privacidad.pdf'); ?>" target="_blank">Política de Privacidad</a></li>
         <li><a href="<?php echo site_url('/media/bases-legales.pdf'); ?>" target="_blank">Bases</a></li>
     </ul>
 </footer>
