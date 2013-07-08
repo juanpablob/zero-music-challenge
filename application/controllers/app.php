c274faa16b6c4009d397cd2fc16ebe75f0c837bd
Juan Pablo [Lipibook] <m.juanpablob@gmail.com>
2013-07-07 20:09:14 -0400

    signed request debug

--- application/controllers/app.php
+++ application/controllers/app.php
@@ -82,6 +82,7 @@
         */
         public function step01() {
             if(isset($_POST['signed_request'])) {
+                echo 'si';
                 $signed_request = $this->facebook->parse_signed_request($_POST['signed_request'], $this->config->item('secret', 'facebook'));
                 
                 print_r($signed_request);
