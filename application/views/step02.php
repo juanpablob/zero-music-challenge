5eaaa2320c8bfe9b91bf3de51b7abb76d560a775
Juan Pablo [Lipibook] <m.juanpablob@gmail.com>
2013-07-07 22:42:30 -0400

    instructions changes

--- application/views/step02.php
+++ application/views/step02.php
@@ -8,8 +8,8 @@
                     <span>Antes de participar debes saber:</span>
                     <p>Tendrás que acertar a la canción que estás escuchando dentro de las cuatro alternativas que aparecerán en pantalla.</p>
                     <p>Cada respuesta correcta te dará puntos que ser irán acumulando en tu cuenta.</p>
-                    <p>Las dos personas con mayor puntaje serán los seleccionados para la gran final en los estudios de Radio Zero en un programa especial que probará tus conocimientos musicales.</p>
-                    <p>El premio para el ganador será un iPod® Shuffle de 2 GB.</p>
+                    <p>Las 4 personas con mayor puntaje serán los seleccionadas para la gran final en los estudios de Radio Zero en un programa especial que probará tus conocimientos musicales.</p>
+                    <p>El premio para el ganador será un iPad Mini de 16GB y para los demás un iPod® Shuffle de 2 GB.</p>
                 </div>
                 
                 <a href="<?php echo site_url(array('app', 'step03')); ?>" class="button">¡A Jugar!</a>
