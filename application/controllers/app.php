503d7429c000fabc5ca90c5432b27f8e031c583c
Juan Pablo [Destacador's iMac] <juanpablo@plumon.cl>
2013-07-25 23:40:36 -0400

    register score debug

--- application/controllers/app.php
+++ application/controllers/app.php
@@ -239,9 +239,7 @@
             $this->session->set_userdata('correct_answers', 4 - $answers['minus']);
             
             // Register score instance
-            //$this->score_model->register_score($this->session->userdata['user_info']->id, $this->session->userdata['user_info']->score);
-            echo 'user id: ' . $this->session->userdata['user_info']->id;
-            echo '<br> score: '. $this->session->userdata['user_info']->score;
+            $this->register_score($this->session->userdata['user_info']->id, $this->session->userdata['user_info']->score);
         }
         
         public function test() {
@@ -253,9 +251,6 @@
         |-------------------------------------------
         */
         public function register_score($user_id, $score) {
-            echo 'method register<br>';
-            print_r($user_id);
-            print_r($score);
             $data = array(
                 'user_id' => $user_id,
                 'score' => $score
--- application/models/score_model.php
+++ application/models/score_model.php
@@ -12,12 +12,12 @@
             $this->db->insert('scores', $data);
         }
         
-        /*public function get_top_week() {
+        public function get_top_week() {
             $this->db->select('(SELECT SUM(score) FROM scores ORDER BY WEEK(created), user_id, GROUP BY user_id, WEEK(created)'), false)
             $query = $this->db->get('scores');
             
             print_r($query);
-        }*/
+        }
         
     }
     
